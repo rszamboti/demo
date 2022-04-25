b702e118a6f5f8dfee7ca3bdc8d6868ac2c3922d
Ricardo Zamboti <54674387+rszamboti@users.noreply.github.com>
2022-04-25 07:42:46 -0300

    Update terraform.tf

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   cloud {
-    organization = "fvilarinho"
+    organization = "git-demo"
     workspaces {
       name = "demo"
     }
@@ -37,13 +37,13 @@ resource "digitalocean_droplet" "cluster-manager" {
       "apt -y install curl wget htop unzip dnsutils",
       "export K3S_TOKEN=${var.digitalocean_token}",
       "curl -sfL https://get.k3s.io | sh -",
-      "kubectl apply -n portainer -f https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer-lb.yaml",
-      "export DD_AGENT_MAJOR_VERSION=7",
-      "export DD_API_KEY=${var.datadog_agent_key}",
-      "export DD_SITE=datadoghq.com",
-      "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
-      "chmod +x ./install_script.sh",
-      "./install_script.sh"
+      "kubectl apply -n portainer -f https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer-lb.yaml" #,
+#      "export DD_AGENT_MAJOR_VERSION=7",
+#      "export DD_API_KEY=${var.datadog_agent_key}",
+#      "export DD_SITE=datadoghq.com",
+#      "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
+#      "chmod +x ./install_script.sh",
+#      "./install_script.sh"
     ]
 
     connection {
