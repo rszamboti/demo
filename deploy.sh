eb15575c97468a0af99968433d3ab833ce8b412b
Ricardo Zamboti <54674387+rszamboti@users.noreply.github.com>
2022-04-25 07:41:18 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -42,8 +42,8 @@ $TERRAFORM_CMD init
 $TERRAFORM_CMD apply -auto-approve \
                      -var "digitalocean_token=$DIGITALOCEAN_TOKEN" \
                      -var "digitalocean_public_key=$DIGITALOCEAN_PUBLIC_KEY" \
-                     -var "digitalocean_private_key=$DIGITALOCEAN_PRIVATE_KEY" \
-                     -var "datadog_agent_key=$DATADOG_AGENT_KEY"
+                     -var "digitalocean_private_key=$DIGITALOCEAN_PRIVATE_KEY" 
+ #                    -var "datadog_agent_key=$DATADOG_AGENT_KEY"
 
 # Get the IP of the cluster manager used to deploy the application.
 export CLUSTER_MANAGER_IP=$($TERRAFORM_CMD output -raw cluster-manager-ip)
